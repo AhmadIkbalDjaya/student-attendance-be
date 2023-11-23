2c654b9b6466cff6e7892c30b81a5f5f5eafba75
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-11-23 13:00:13 +0800

    fix: teacher home enpoint(active semester only)

--- app/Http/Controllers/Teacher/TeacherHomeController.php
+++ app/Http/Controllers/Teacher/TeacherHomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Attendance;
 use App\Models\Claass;
 use App\Models\Course;
+use App\Models\Semester;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,20 +15,24 @@ class TeacherHomeController extends Controller
 {
     public function index()
     {
-        $course_count = Course::where("teacher_id", auth()->user()->teacher->id)->count();
-        $claass_count = Claass::whereHas("courses", function ($q) {
-            $q->where("teacher_id", auth()->user()->teacher->id);
+        $active_semester_id = Semester::where("is_active", 1)->pluck('id')->first();
+        $active_semester_id = $active_semester_id == null ?  0 : $active_semester_id;
+        $course_count = Course::where("teacher_id", auth()->user()->teacher->id)
+            ->where("semester_id", $active_semester_id)
+            ->count();
+        $claass_count = Claass::whereHas("courses", function ($q) use ($active_semester_id) {
+            $q->where("teacher_id", auth()->user()->teacher->id)->where("semester_id", $active_semester_id);
         })->count();
-        $student_count = Student::whereHas("claass", function ($q) {
-            $q->whereHas("courses", function ($q) {
-                $q->where("teacher_id", auth()->user()->teacher->id);
+        $student_count = Student::whereHas("claass", function ($q) use ($active_semester_id) {
+            $q->whereHas("courses", function ($q) use ($active_semester_id) {
+                $q->where("teacher_id", auth()->user()->teacher->id)->where("semester_id", $active_semester_id);
             });
         })->count();
-        $attendance_count = Attendance::whereHas("course", function ($q) {
-            $q->where("teacher_id", auth()->user()->teacher->id);
+        $attendance_count = Attendance::whereHas("course", function ($q) use ($active_semester_id) {
+            $q->where("teacher_id", auth()->user()->teacher->id)->where("semester_id", $active_semester_id);
         })->count();
-        $attendance_null_count = Attendance::whereHas("course", function ($q) {
-            $q->where("teacher_id", auth()->user()->teacher->id);
+        $attendance_null_count = Attendance::whereHas("course", function ($q) use ($active_semester_id) {
+            $q->where("teacher_id", auth()->user()->teacher->id)->where("semester_id", $active_semester_id);
         })->where("is_filled", false)->count();
         return response()->json([
             "data" => [
